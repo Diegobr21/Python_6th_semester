d4acf9ec0f1a1087a97a17ba45825c20a0b4b894
Diegobr21 <jdiegobrana@gmail.com>
2020-03-20 01:34:49 -0600

    Constructive and Local Saerch Heuristic with Greedy Start and Vertex Susbstitution

--- TSO/PMP/p_heur.py
+++ TSO/PMP/p_heur.py
@@ -129,9 +129,154 @@ def greedy_ls(p):
 
     print('Total distance/cost covered: ',totdist)
 
+#?Hybrid heuristic with Greedy start and Vertex Substitution
+def greedy_vs(p,m):
+    x=0
+    dist_ij=[]
+    for e in data[1:]:        
+        x=x+1
+        dist_ij.append(data[x])
+   
+    dij=[]
+    for e in dist_ij:
+        dij.append(tuple(map(int, e.split())))
+    #List of distances_ij already integer values
+    list_sum_dij=[]
+    
+    #Sum for each to facility to all customers
+    for i in range(len(dij)):
+        list_sum_dij.append( sum(dij[i])-i )
+
+    ind=[i for i in range(m)]
+    #List of sums with index
+    listsumindex= merge(ind, list_sum_dij)
+    #From smallest to greatest sum
+    lsum=sorted(listsumindex, key=lambda i: i[1], reverse=False)
+
+    #Append the facility which which most reduces total cost/distance until the p criteria is met
+    F=[] 
+    x=0
+    totdist=0
+    totcust=0
+    p1=p
+    while p1!=0:
+        F.append(lsum[x][0])
+        totdist+=lsum[x][1]
+        x+=1
+        p1-=1
+    
+    #totdist=distance covered without VS
+    #?Vertex Substitution START
+    #*Create as many subsets of Vi as there are customers
+    V = [[] for i in range(n)]
+    
+    for j in range(n):#through customers
+        for i in range(m):#through facilities
+            V[j].append(dij[i][j+1])
+    #V[0]=distances from customer 0 to every facility 
+    
+    #*Assign the customers to their closest facility
+    closest_f=[]     #List of which facility is closest to each customer
+    for subset in V:
+        closest_f.append(subset.index(min(subset)))
+    
+    result = [item for items, c in Counter(closest_f).most_common() for item in [items] * c] 
+    #result = Ordered list with facilities repeating as many times as they were chosen as the closest
+                                       
+    lcust=[]
+    l_times=[]
+    c=0
+    k=result[0]
+    for i in range(len(result)):
+        if k == result[i]:
+            c+=1
+        else:
+            lcust.append(k)
+            l_times.append(c)
+            k=result[i]
+            c=1
+            if k==result[-1]:
+                lcust.append(k)
+                l_times.append(1)
+
+    assign_f=merge(lcust,l_times) #Ordered list with facilities and how many times they were chosen as the closest
+    #?totcust+=customers before Local Search
+    for f in F:
+        if f in lcust:
+            totcust+=assign_f[lcust.index(f)][1]
+
+    #print(lsum,"\n")
+    #print(assign_f,"\n") 
+    #print(lcust,"\n")    
+    print(f"Greedy Start: F={F} with distance covered = {totdist}, and customers close to their facilities = {totcust} ")
+    sumacust=0
+    for i in range(len(assign_f)):
+        sumacust+=assign_f[i][1]
+    avg_cust=int(round(sumacust/m))#minimum amount of customers a facility should have to be opened
+    print('avgcust ', avg_cust)
+    z=0
+
+    for i in range(len(F)):
+        
+        for j in lsum[p:]:
+            if F[i] in lcust and assign_f[lcust.index(F[i])][1] < avg_cust:
+                if j[0] in lcust and assign_f[lcust.index(j[0])][1] >= avg_cust:
+                    totdist-=listsumindex[F[i]][1]
+                    totcust-=assign_f[lcust.index(F[i])][1]
+                    F.pop(i)
+                    F.insert(i,j[0]) 
+                    totdist+=listsumindex[F[i]][1]
+                    totcust+=assign_f[lcust.index(F[i])][1]
+                    
+                    lsum.pop(lsum.index(j))
+            elif F[i] not in lcust:
+                if j[0] in lcust and assign_f[lcust.index(j[0])][1] >= avg_cust:
+                    totdist-=listsumindex[F[i]][1]
+                    F.pop(i)
+                    F.insert(i,j[0]) 
+                    totdist+=listsumindex[F[i]][1]
+                    totcust+=assign_f[lcust.index(F[i])][1]
+                    lsum.pop(lsum.index(j))
 
 
 
+
+
+        """  
+        if F[i] in lcust:
+            if assign_f[lcust.index(F[i])][1] < avg_cust:#si tiene menos clientes que el minimo, cambiar
+                if lsum[p+z][0] in lcust and assign_f[lcust.index(lsum[p+z][0])][1] >= avg_cust: #Check if the next facility with least sum of distance has >= customers than avg
+                    totdist-=listsumindex[F[i]][1]
+                    F.pop(i)
+                    F.insert(i,lsum[p+z][0]) 
+                    totdist+=listsumindex[F[i]][1]
+                    z+=1
+                elif lsum[p+z][0] in lcust and assign_f[lcust.index(lsum[p+z][0])][1] < avg_cust:
+
+        else:
+            if lsum[p+z][0] in lcust and assign_f[lcust.index(lsum[p+z][0])][1] >= avg_cust: #Check if the next facility with least sum of distance has >= customers than avg
+                    totdist-=listsumindex[F[i]][1]
+                    F.pop(i)
+                    F.insert(i,lsum[p+z][0]) 
+                    totdist+=listsumindex[F[i]][1]
+                    z+=1
+
+        except ValueError:
+            print('next')
+            
+            if assign_f[lcust.index(lsum[p+z][0])][1] >= avg_cust: #Check if the next facility with least sum of distance has >= customers than avg
+                    totdist-=listsumindex[F[i]][1]
+                    F.pop(i)
+                    F.insert(i,lsum[p+z][0]) 
+                    totdist+=listsumindex[F[i]][1]
+                    z+=1
+        """
+    print(f"After VS: F={F} with distance covered = {totdist}, and customers close to their facilities = {totcust}")
+            
+
+
+
+#*-------------------------------------------------------------------------------------------------------------*#
 print('.txt files in directory\n')
 for file in os.listdir(dirname(os.path.realpath(__file__))):#os.path.realpath(__file__)
     if file.endswith(".txt"):
@@ -174,14 +319,17 @@ try:
     p=int(p)
 
     try:
-        choice=int(input('Enter 1 for Local search or 2 for doing it without ls: '))
+        choice=int(input('Enter:\n1 for Greedy Heuristic\n2 for Local Search maximizing customers per facility with Greedy Start\n3 for Hybrid Heuristic Greedy + VS\n'))
         if choice==1:
-            greedy_ls(p)
-        else:
             greedy_p(p)
+        elif choice==2:
+            greedy_ls(p)
+        elif choice==3:
+            greedy_vs(p,m)
     except:
         print("Running it without local search then")
-        greedy_p(p)
+        #greedy_p(p)
         
 except:
     print('data not processed')
+#greedy_vs(p,m)
