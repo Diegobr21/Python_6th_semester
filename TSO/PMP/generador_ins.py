77759406a1fcccf6b33c7626d6b662e565c7106e
Diegobr21 <jdiegobrana@gmail.com>
2020-05-02 18:24:25 -0500

    generador_ins now writes the average distance

--- TSO/PMP/generador_ins.py
+++ TSO/PMP/generador_ins.py
@@ -1,4 +1,5 @@
 import random as random
+import numpy as np
 
 p=int(input('Enter the number of facilities to be opened: '))
 m=int(input('Enter the number of facilities available: '))
@@ -7,6 +8,9 @@ dmin=int(input('Smallest distance possible from customer(i) to facility(j): '))
 dmax=int(input('Greatest distance possible from customer(i) to facility(j): '))
 if dmin < dmax and dmin > 0 and dmax > 0 and p <= m and m > 0:
     print('input successful')
+    mu = (dmin + dmax) / 2     
+    data = mu + 2.5*np.random.randn(m, n)
+    #print(data)
     
                
     filename=input('Enter a filename or press Enter to leave it as instancia: ')
@@ -16,18 +20,20 @@ if dmin < dmax and dmin > 0 and dmax > 0 and p <= m and m > 0:
         filename=' '
     if filename!=' ':
         with open(filename,'w') as f:
-            f.write('%r %r %r' % (m, n, p))
+            f.write('%r %r %r %r' % (m, n, p, int(mu)))
             f.write('\n')
 
             for i in range(m):
                 f.write('%r' % (i)+ " ")
+                
                 for j in range(n):
-                    f.write('%r' % (random.randint(dmin, dmax))+ " ")
+                    f.write('%r' % (data[i][j])+ " ")
                 f.write('\n')
+                
             f.close()
     else:
         with open('instancia.txt', 'w') as f:    
-            f.write('%r %r %r' % (m, n, p))
+            f.write('%r %r %r %r' % (m, n, p, int(mu)))
             f.write('\n')
 
             for i in range(m):
@@ -37,5 +43,7 @@ if dmin < dmax and dmin > 0 and dmax > 0 and p <= m and m > 0:
                 f.write('\n')
             f.close()
             
+        
 else:
     print('input failed')
+
